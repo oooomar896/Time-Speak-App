367aaa8c295f7536aa95df9b4372a0894e737b1c
oooomar896 <43324647+oooomar896@users.noreply.github.com>
2024-12-08 08:57:29 +0300

    m

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,18 @@ import 'package:admin_alghozami/routes.dart';
 import 'package:admin_alghozami/view/screen/langauge/localization/changelocal.dart';
 import 'package:admin_alghozami/view/screen/langauge/localization/translilation.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
 import 'package:get/get.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initialServices();
-  await initialServices();
   runApp(const MyApp());
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  print("App Name: ${packageInfo.appName}");
+  print("Package Name: ${packageInfo.packageName}");
+  print("SHA Info is not directly accessible in Flutter. Use Gradle/Keytool.");
 }
 
 class MyApp extends StatelessWidget {
@@ -31,6 +36,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-//user:swap_user-password:qVbLtuaUsKoB5qQn
-//- SHA1:
-//- SHA256:
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import connectivity_plus
 import nb_utils
+import package_info_plus
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   NbUtilsPlugin.register(with: registry.registrar(forPlugin: "NbUtilsPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
