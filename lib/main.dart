ae8fec48386f596479929bf1ee34d48c8e2053ff
oooomar896 <43324647+oooomar896@users.noreply.github.com>
2024-12-08 11:03:05 +0300

    n

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,13 @@ import 'package:admin_alghozami/routes.dart';
 import 'package:admin_alghozami/view/screen/langauge/localization/changelocal.dart';
 import 'package:admin_alghozami/view/screen/langauge/localization/translilation.dart';
 import 'package:flutter/material.dart';
-import 'package:package_info_plus/package_info_plus.dart';
-
 import 'package:get/get.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initialServices();
   runApp(const MyApp());
-  PackageInfo packageInfo = await PackageInfo.fromPlatform();
-  print("App Name: ${packageInfo.appName}");
-  print("Package Name: ${packageInfo.packageName}");
-  print("SHA Info is not directly accessible in Flutter. Use Gradle/Keytool.");
+
 }
 
 class MyApp extends StatelessWidget {
@@ -27,12 +22,11 @@ class MyApp extends StatelessWidget {
      return GetMaterialApp(
       color: Colors.white,
       translations: MyTranslation(),
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,          
       locale: controller.langauge,
       theme: controller.appTheme,
       initialBinding: InitialBinding(),
       getPages: routes,
-    // home: MyCustomWidget(),
     );
   }
 }
