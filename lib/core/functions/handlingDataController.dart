ee813e25f1c322eb85803bb457364e01c2d9068d
oooomar896 <43324647+oooomar896@users.noreply.github.com>
2024-12-08 11:05:40 +0300

    n

--- lib/core/functions/handlingDataController.dart
+++
@@ -1,9 +0,0 @@
-import 'package:admin_alghozami/core/class/statusRequest.dart';
-
-handlingData(response) {
-  if (response is StatusRequest) {
-    return response;
-  } else {
-    return StatusRequest.success;
-  }
-}
