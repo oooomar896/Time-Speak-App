15eeb27d04617d188daa0a562cb4d2edd9fa43b0
oooomar896 <43324647+oooomar896@users.noreply.github.com>
2024-12-09 11:41:43 +0300

    m

--- lib/view/screen/OnBoarding.dart
+++ lib/view/screen/OnBoarding.dart
@@ -34,7 +34,7 @@ class _SplashScreenState extends State<OnBoarding> {
                  Column(
                     children: [
                       const SizedBox(height: 300,),
-                 //     Image(image: const AssetImage(),),
+                      Image(image: const AssetImage(AppImageAssets.logo ),),
                        Text("وقت الحديث".tr,style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
                     ],
                 ),
