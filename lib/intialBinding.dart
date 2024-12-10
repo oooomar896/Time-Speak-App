4c9c5dfc7e21857e78cf74ae125cbb2e7e333733
oooomar896 <43324647+oooomar896@users.noreply.github.com>
2024-12-10 10:23:43 +0300

    ة

--- lib/intialBinding.dart
+++ lib/intialBinding.dart
@@ -1,10 +1,11 @@
 
+// ignore_for_file: annotate_overrides
+
 import 'package:get/get.dart';
 import 'package:untitled1/core/services/services.dart';
 import 'package:untitled1/view/screen/home/controller/controller_home.dart';
 
 class InitialBinding extends Bindings {
-  @override
   void dependencies() {
     // تحميل MyServices كـ Singleton طوال عمر التطبيق
     Get.put<MyServices>(MyServices(), permanent: true);
